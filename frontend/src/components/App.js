e79cafc6701c316ec476040264e09ad3bec2f44f
Shim0209 <81503611+Shim0209@users.noreply.github.com>
2021-08-18 20:43:59 +0900

    Update project setting

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import globalStyles from "./globalStyles";
+import Router from './Router';
+
+function App() {
+  return (
+    <>
+      <Router />
+      <globalStyles />
+    </>
+  );
+}
+
+export default App;
